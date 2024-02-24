d653813a1a2269e159b48380a5c55158a0b857ba
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-02-24 07:19:34 +0545

    Basic UI and line chart

---
+++ basics.py
@@ -0,0 +1,45 @@
+from taipy import Gui
+import pandas as pd
+
+title = "Stock Simulator"
+path = "logo.jpeg"
+
+company_name = ""
+min_price = ""
+max_price = ""
+
+data = {
+    "Date": pd.date_range("2024-01-01", periods=4, freq="D"),
+    "Min": [-22, -19.7, 2.7, 6.5],
+    "Max": [-8.6, -8.2, 12.0, 13.5],
+}
+
+
+def stock():
+    print("Hey, hello stock analyst")
+
+
+page = """
+<|text-center |
+<|{path}|image|>
+
+<|{title}|hover_text="Welcome to Stock Screener"|>
+
+Name of Stock: <|{company_name}|input|>
+
+Min Price: <|{min_price}|input|>
+
+Max Price: <|{max_price}|input|>
+
+<|Run Simulation|button|on_action=stock|>
+
+|>
+
+<|{title}|hover_text="Your Simulation"|>
+
+<|{data}|chart|mode=lines|x=Date|y[1]=Temp°C|y[2]=Min|y[3]=Max|line[1]=dash|color[2]=blue|color[3]=red|>
+"""
+
+if __name__ == "__main__":
+    app = Gui(page)
+    app.run(use_reloader=True)
